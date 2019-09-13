bcec8d6c947be27d0e2489ff4694a469f209bc20
Eric Henderson <henderea@gmail.com>
2019-09-13 16:35:35 -0400

    improve type defs

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,15 +1,12 @@
 declare namespace regexUtil {
-    export type processReplace = (args: any[], replaceString: string) => string;
-    export type doReplace = (input: string, matchRegex: RegExp, replaceString: string) => string;
     export interface SubstitutionEntity {
         matchRegex: RegExp;
         replaceString: string;
     }
     export type Many<T> = T | T[]
-    export type doMultiReplace = (input: string, ...subs: Array<Many<SubstitutionEntity>>) => string;
 }
 
 export const XRegExp: any;
-export const processReplace: regexUtil.processReplace;
-export const doReplace: regexUtil.doReplace;
-export const doMultiReplace: regexUtil.doMultiReplace;
+export const processReplace: (args: any[], replaceString: string) => string;
+export const doReplace: (input: string, matchRegex: RegExp, replaceString: string) => string;
+export const doMultiReplace: (input: string, ...subs: Array<regexUtil.Many<regexUtil.SubstitutionEntity>>) => string;
