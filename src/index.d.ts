d800d52c5b781063a657641c7d2cb7a8cb78be41
Eric Henderson <henderea@gmail.com>
2024-11-06 14:34:27 -0500

    add documentation on the syntax

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,50 @@
+/*
+Patterns:
+Uses built-in JavaScript regular expression support. Only indexed capture groups are supported.
+
+Replacement String:
+    The replacement string supports some special match group syntax:
+
+        Basic Syntax:
+            ${1}        insert capture group 1
+            ${1|2}      insert capture group 1, or if that didn't match, insert capture group 2; the | syntax can be
+                        used in all other syntax setups
+
+        Ternary Syntax:
+            ${1?a:b}    insert either 'a' or 'b', depending on if capture group 1 matched anything; a and b can be
+                        empty, and can contain any characters other than : and }
+            ${1?a}      insert 'a' if capture group 1 matched anything, or insert nothing if it did nto match
+
+        Fallback Syntax:
+            ${1:-a}     insert capture group 1, or if that didn't match, insert 'a'
+
+        Substring Syntax:
+            ${1:1}      insert capture group 1, starting at index 1
+            ${1:1:2}    insert capture group 1, starting at index 1, with a length of 2
+
+        Character Case Modification Syntax:
+            ${1^}       insert capture group 1, with the first character converted to uppercase
+            ${1,}       insert capture group 1, with the first character converted to lowercase
+            ${1^^}      insert capture group 1, with the entire capture group converted to uppercase
+            ${1,,}      insert capture group 1, with the entire capture group converted to lowercase
+
+    Syntax notes:
+        • You can nest a replacement string match group syntax in any constant.
+        • You can use the character case modification syntax with any other syntax setups. The case modification syntax goes
+          immediately after the capture group number(s) from the basic syntax.
+
+        Examples:
+            ${1?${2}${3}}             insert capture group 2 if group 1 was matched, or group 3 if it didn't
+            ${1^^?${2}${3}}           insert capture group 2 if group 1 was matched, or group 3 if it didn't. either
+                                      way, make the entire string uppercase
+            ${1?${2^^}${3|4,,}}       insert capture group 2 all uppercase if group 1 was matched. If capture group 1
+                                      didn't match, insert group 3 (or 4 if 3 didn't match) all lowercase
+            ${1:-${2}}                same as ${1|2}
+            ${1:${2?1:2}:${2?3:2}}    insert a substring of capture group 1; if capture group 2 matched, the substring
+                                      will be 3 characters starting at index 1; if capture group 2 did not match, the
+                                      substring will be 2 characters starting at index 2
+*/
+
 declare namespace regexUtil {
     export interface SubstitutionEntity {
         matchRegex: RegExp;
